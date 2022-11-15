cc1b9d9211552d136df8bd3fe8e3cab68b5e59fc
Juan Oscco <juan.osccomori@gmail.com>
2022-11-14 22:53:50 -0500

    subiendo datos del quinto ejercicio

---
+++ fifth-exercise/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ComponentFormik } from "./components";
+
+export const App = () => {
+    return (
+        <div>
+            <ComponentFormik/>
+        </div>
+    );
+}
---
+++ fifth-exercise/src/components/index.js
@@ -0,0 +1,1 @@
+export { default as ComponentFormik } from "./Form/Form";
